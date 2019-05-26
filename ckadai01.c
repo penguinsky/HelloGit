186dae9f9e18ca6701f5d75fdbcde05069ef07ae
penguinsky <kd1305552@st.kobedenshi.ac.jp>
2019-05-26 10:33:28 +0900

    aaaaa

--- ckadai01.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main(void)
-{	
-	printf("aaaaa\n");
-	
-	return 0;
-}
---
+++ ckadai03.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int a=50,b=10;
+	
+	printf("a+b=%d\n",a+b);
+	printf("a-b=%d\n",a-b);
+	printf("a*b=%d\n",a*b);
+	printf("a/b=%d\n",a/b);
+	
+	return 0;
+}
